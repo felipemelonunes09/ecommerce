13adbbeb2faf397e3c693ec5b4c1d193abe8e28d
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-25 14:09:26 -0300

    raw: added the title and version of swaggerDoc

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title="Catalog", Version="v1"}); });
 
 
 var app = builder.Build();
