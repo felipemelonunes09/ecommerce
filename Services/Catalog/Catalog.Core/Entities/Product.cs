2817be93b33d2df578d0af2fda686f385e8e688b
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-25 14:31:53 -0300

    feat: added base class and product class

---
+++ Services/Catalog/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities
+{
+    public abstract class Product : BaseEntity
+    {
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public ProductBrand Brands { get; set; }
+        public ProductType Type { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+    }
+}
