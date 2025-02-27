efcfe7e30a5f08c262dd39f334716be18e8c251d
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-27 16:11:37 -0300

    raw: add a space

--- Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
+++ Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
@@ -6,7 +6,7 @@ namespace Catalog.Application.Mappers
 {
     public class ProductMapper
     {
-        private static readonly Lazy<IMapper> Lazy= new Lazy<IMapper>(() => {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() => {
             var config = new MapperConfiguration(cfg => {
                 cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
                 cfg.AddProfile<ProductMappingProfile>();
