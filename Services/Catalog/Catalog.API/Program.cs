700001f1e35fb2957c2f4159e1c7b4f6891379fe
felipe-melo <felipe.melonunes09@gmail.com>
2025-03-07 17:33:10 -0300

    feat: added services

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,24 @@
+using System.Reflection;
+using Catalog.Core.Repositories;
+using Catalog.Infra.Data;
+using Catalog.Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title="Catalog.API", Version="v1"}); });
 
+
+// register automapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+// register mediatr
+builder.Services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+// registar application services
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IBrandRepository, ProductRepository>();
+builder.Services.AddScoped<ITypesRepository, ProductRepository>();
 
 var app = builder.Build();
 
