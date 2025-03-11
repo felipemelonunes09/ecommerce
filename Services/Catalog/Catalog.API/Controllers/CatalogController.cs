47a98bb9aa2e7223c9b5d139cb22e639d1c91c7e
felipe-melo <felipe.melonunes09@gmail.com>
2025-03-11 18:08:16 -0300

    feat: added route to get all products

---
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace Catalog.API.Controllers {
+    public class CatalogController:ApiController {
+        private readonly IMediator mediator;
+        public CatalogController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }   
+
+        [HttpGet]
+        [Route("[action]/{id}", Name="GetProductById")]
+        [ProducesResponseType(typeof(ProductResponse), (int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ProductResponse>> GetProductById(string id) {
+            var query = new GetProductByIdQuery(id);
+            var result = await this.mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]/{name}", Name="GetProductByName")]
+        [ProducesResponseType(typeof(ProductResponse), (int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ProductResponse>> GetProductByName(string name) {
+            var query = new GetProductByNameQuery(name);
+            var result = await this.mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetAllProducts")]
+        [ProducesResponseType(typeof(ProductResponse), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ProductResponse>> GetAllProducts()
+        {
+            var query = new GetAllProductsQuery();
+            var result = await this.mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
